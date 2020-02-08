98285910aee99378b8db1626e9ab7eb2094235f5
FUAD\fuady <fuadmyoussef@gmail.com>
2020-02-08 04:02:13 -0500

    made some progress

---
+++ backend/client.js
@@ -0,0 +1,9 @@
+//client.js
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000', {reconnect: true});
+
+// Add a connect listener
+socket.on('connect', function (socket) {
+    console.log('Connected!');
+});
+socket.emit('CH01', 'me', 'test msg');
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,28 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+let roomCodes = [];
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+    var nsp;
     console.log('a user connected');
-    socket.on('username', function(msg){
+    socket.on('create', function(msg, usr){
         io.emit('username', msg);
+        roomCodes.push(msg);
+        nsp = io.of('/' + msg);
+        socket.join(msg);
         console.log(msg)
+        console.log(usr)
     });
-})
+    socket.on('roomcode', function(msg, usr){
+        io.emit('roomcode', msg);
+        console.log(msg);
+        socket.join(msg);
+    });
+});
 
 http.listen(3000,function(){
     console.log('listening on *:3000');
