f2f74d2132bbac0a37797be9be5010e6ebaa711f
FUAD\fuady <fuadmyoussef@gmail.com>
2020-02-08 12:33:37 -0500

    transfering arrays

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 let roomCodes = [];
+var Rooms = [];
 app.use(express.static('public'));
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
@@ -12,27 +13,40 @@ app.get('/wait', function(req,res){
 });
 
 io.on('connection', function(socket){
-    var nsp;
+    const toSendJSON = JSON.stringify(roomCodes);
+    console.log("to send json ", toSendJSON);
+    socket.emit("room update", toSendJSON);
     console.log('a user connected');
     socket.on('create', function(msg, usr){
-
         if (!roomCodes.includes(msg.roomcode)) {
             roomCodes.push(msg.roomcode);
+            var newRoom = {roomcode: msg.roomcode, players: [msg.username]};
+            console.log(newRoom);
+            Rooms.push(newRoom);
+            console.log(Rooms);
             io.emit('createresp', msg);
-            io.to(socket.id).emit('enterroom', msg);
-            console.log(roomCodes)
-            socket.join(msg);
-            console.log(msg)
-            console.log(usr)
+            io.to(socket.id).emit('enterroom', msg, newRoom);
+            socket.join(msg.roomcode);
         }
     });
     socket.on('join', function(msg, usr){
         if (roomCodes.includes(msg.roomcode)) {
             io.emit('joinresp', msg);
-            console.log(roomCodes)
-            socket.join(msg);
-            console.log(msg)
-            console.log(usr)
+            var newRoom;
+            console.log("join");
+            console.log(Rooms);
+            for(r in Rooms) {
+                console.log(r);
+                console.log("made it here1");
+                if (r["roomcode"] === msg.roomcode) {
+                    r.players.push(msg.username);
+                    console.out("made it here")
+                    newRoom = r;
+                }
+                io.to(socket.id).emit('enterroom', msg, newRoom);
+                socket.join(msg.roomcode);
+            }
+
         }
     });
 });
--- backend/public/client.js
+++ backend/public/client.js
@@ -20,16 +20,38 @@ socket.on('createresp', function (msg) {
     node.appendChild(textnode);                              // Append the text to <li>
     rooms.appendChild(node);     // Append <li> to <ul> with id="myList"
 });
+socket.on('room update', function (msg) {
+    console.log("msg is " + msg );
+    pmsg = JSON.parse(msg);
+    console.log("pmsg is ", pmsg );
+    console.log("pmsg 0", pmsg[0]);
+    for (m in pmsg) {
+        console.log("printing " + m)
+        var rooms = document.getElementById("rooms");
+        var node = document.createElement("LI");                 // Create a <li> node
+        var textnode = document.createTextNode(m);         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        rooms.appendChild(node);
+    }
+});
+
 socket.on('joinresp', function (msg) {
-    var rooms = document.getElementById("rooms");
+   /* var rooms = document.getElementById("rooms");
     var node = document.createElement("LI");                 // Create a <li> node
     var textnode = document.createTextNode(msg.roomcode);         // Create a text node
     node.appendChild(textnode);                              // Append the text to <li>
-    rooms.appendChild(node);     // Append <li> to <ul> with id="myList"
+    rooms.appendChild(node);   */  // Append <li> to <ul> with id="myList"
 });
 
-socket.on('enterroom', function (msg) {
-    ui.innerHTML='<p><strong>'+msg.roomcode+" "+msg.username+'</strong></p>'
+socket.on('enterroom', function (msg, newRoom) {
+    ui.innerHTML='<p><strong>'+"Welcome "+msg.username+ " to room " + msg.roomcode+" Your fellow players are: "+'</strong></p><br><ul id="players"></ul>'
+    for (p in newRoom.players) {
+        var rooms = document.getElementById("players");
+        var node = document.createElement("LI");                 // Create a <li> node
+        var textnode = document.createTextNode(p);         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        rooms.appendChild(node);     // Append <li> to <ul> with id="myList"
+    }
 });
 
 
